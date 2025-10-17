12c844c0110de1fb8d57653273fd5415a99543a3
KarolKaszuba <126276932+KarolKaszuba@users.noreply.github.com>
2025-10-17 19:06:17 +0200

    Integrate OneButton library for button interactions
    Refactor code to use OneButton library for button handling and improve display management.

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -1,15 +1,21 @@
-#include <LiquidCrystal.h> //Dołączenie bilbioteki
+#include <LiquidCrystal.h>
+#include <OneButton.h>
 
 #define P1_RESISTOR 8
 #define P2_DIODE 9
 #define P3_TRANSISTOR 10
-void startInfo();
 
-int lastState1 = HIGH; // bo pullup | chce łapać zbocze rosnące
+
+void startInfo();
+void cleanAndSetDisp();
+void startInfo();
+void handleClick();
+void handleDoubleClick();
+
+OneButton P1_res = OneButton(P1_RESISTOR, true); // true bo aktuwuje gdy LOW
 int lastState2 = HIGH; // bo pullup | chce łapać zbocze rosnące
 int lastState3 = HIGH; // bo pullup | chce łapać zbocze rosnące
-int counterP3 = 0; // do podwójnego kliknięcia (wyjscie z trybów)
-unsigned long pushTime = 0; //do mierzenia czasu wcisniecia przycisku
+
 
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7); // podłączenie wyswietlacza do pinów 2 (RS), 3 (Enable), 4 (D4), 5 (D5), 5 (D6) i 7 (D7).
 
@@ -19,7 +25,42 @@ void setup() {
   pinMode(P1_RESISTOR , INPUT_PULLUP); //Przycisk jako wejście
   pinMode(P2_DIODE , INPUT_PULLUP);
   pinMode(P3_TRANSISTOR , INPUT_PULLUP);
+
+  P1_res.attachClick(handleClick);
+  P1_res.attachDoubleClick(handleDoubleClick);
 }
+
+void loop() {
+
+  P1_res.tick();
+
+  int currentState2 = digitalRead(P2_DIODE);
+  int currentState3 = digitalRead(P3_TRANSISTOR);
+
+
+  
+  if(lastState2 == LOW and currentState2 == HIGH){
+    cleanAndSetDisp();
+    lcd.print("DIODE MODE");
+    delay(500);
+  }
+  
+  if(lastState3 == LOW and currentState3 == HIGH){
+    cleanAndSetDisp();
+    lcd.print("TRANSISTOR MODE");
+    delay(500);
+  }
+
+  lastState2 = currentState2;
+  lastState3 = currentState3;
+  delay(50);
+}
+
+
+
+
+
+
 void cleanAndSetDisp(){
   lcd.clear();
   lcd.setCursor(0,0);
@@ -37,37 +78,13 @@ void startInfo(){
   //lcd.clear();
 }
 
-void loop() {
-  int currentState1 = digitalRead(P1_RESISTOR);
-  int currentState2 = digitalRead(P2_DIODE);
-  int currentState3 = digitalRead(P3_TRANSISTOR);
+void handleClick(){
+  cleanAndSetDisp();
+  lcd.print("RESISTOR MODE");
+  delay(500);
+}
 
-  if(lastState1 == HIGH and currentState1 == LOW){
-    pushTime = millis();
-    
-    if(pushTime >= 2000){
-      startInfo();
-    }
-  }
-  else if(lastState1 == LOW and currentState1 == HIGH){
-      cleanAndSetDisp();
-      lcd.print("RESISTOR MODE");
-      delay(500);
-  }
-  else if(lastState2 == LOW and currentState2 == HIGH){
-    cleanAndSetDisp();
-    lcd.print("DIODE MODE");
-    delay(500);
-  }
-  else if(lastState3 == LOW and currentState3 == HIGH){
-    cleanAndSetDisp();
-    lcd.print("TRANSISTOR MODE");
-    delay(500);
-  }
-  lastState1 = currentState1;
-  lastState2 = currentState2;
-  lastState3 = currentState3;
-  delay(50);
-  
-
+void handleDoubleClick(){
+  startInfo();
+  delay(500);
 }
